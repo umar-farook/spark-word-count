fa3db821231e7a90d73839b70b1fcf0262f351c6
Umar Farook <mohd.umarfarook2008@gmail.com>
2019-10-26 20:20:06 +0530

    Added Word Count

--- src/main/scala/com/spark/babysteps/Main.scala
+++ src/main/scala/com/spark/babysteps/Main.scala
@@ -1,6 +1,18 @@
 package com.spark.babysteps
 
-object Main  extends App{
-  println("Hello Spark")
- 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions._
+import org.apache.log4j.Level;
+
+object Main extends App {
+  //Sum of 10
+  org.apache.log4j.Logger.getLogger("org").setLevel(Level.ERROR)
+  val spark = SparkSession.builder().master("local[2]").appName("WordCount").getOrCreate();
+  import spark.implicits._
+  val data = spark.sparkContext.textFile(args(0))
+  //Top 10 Words
+  val result = data.flatMap(_.split("\\W+")).map(_ -> 1).reduceByKey(_ + _).map(x => x._2 -> x._1).sortByKey(false).map(x => s"${x._2} : ${x._1}").take(10)
+  result.foreach(println)
+
 }
